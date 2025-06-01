caa370cdf77b9d9450720d3f1897d09321be815e
Carlos Martinez <camartinez7@hotmail.com>
2025-05-31 19:10:35 -0500

    DTO

---
+++ SpringBootRedisDocker/src/main/java/com/example/SpringBootRedisDocker/ProductDto.java
@@ -0,0 +1,9 @@
+package com.example.SpringBootRedisDocker;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record ProductDto(Long id, @NotBlank String name, @Positive BigDecimal price) {
+}
---
+++ SpringBootRedisDocker/src/main/java/com/example/SpringBootRedisDocker/redis/settings/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.example.SpringBootRedisDocker.redis.settings;
+
+import com.example.SpringBootRedisDocker.ProductDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration  // Marks this class as a Spring configuration class
+public class RedisConfig {
+    @Bean  // Defines a Spring bean for RedisCacheManager
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // Define cache configuration
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) // Set time-to-live (TTL) for cache entries to 10 minutes
+                .disableCachingNullValues() // Prevent caching of null values
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(ProductDto.class))); // Serialize values using Jackson JSON serializer
+
+        // Create and return a RedisCacheManager with the specified configuration
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig) // Apply default cache configuration
+                .build();
+    }
+}
