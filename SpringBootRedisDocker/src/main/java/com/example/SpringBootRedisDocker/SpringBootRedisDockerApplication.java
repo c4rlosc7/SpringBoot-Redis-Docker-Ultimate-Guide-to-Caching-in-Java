1eabc5c706ef9e5ac31864571942b10d4606a19b
Carlos Martinez <camartinez7@hotmail.com>
2025-05-31 19:02:05 -0500

    init project SB

---
+++ SpringBootRedisDocker/src/main/java/com/example/SpringBootRedisDocker/SpringBootRedisDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRedisDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisDockerApplication.class, args);
+	}
+
+}
